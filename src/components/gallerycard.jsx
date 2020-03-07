c121689cce150f00fdc92996525b455aa9d38e69
Sam Delaney <sam@thedelaneys.net>
2020-03-06 20:19:23 -0800

    modal ui v1

--- src/components/gallerycard.jsx
+++ src/components/gallerycard.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import {Text, Image, OverflowSet, Modal, Link} from 'office-ui-fabric-react';
+import {Text, Image, ImageFit, OverflowSet, Modal, Link, mergeStyleSets, IconButton, getTheme, FontSizes, FontWeights} from 'office-ui-fabric-react';
 import {Card} from '@uifabric/react-cards';
+import DragScrollProvider from 'drag-scroll-provider';
 
 var images = require.context("../images", true)
-
-const footerCardSectionStyles = {
-    root: {
-      borderTop: '1px solid #F3F2F1'
-    }
-};
-
-const footerCardSectionTokens = { padding: '12px 0px 0px' };
+var theme = getTheme()
 
 class GalleryCard extends React.Component {
     state = {
@@ -24,13 +18,42 @@ class GalleryCard extends React.Component {
         }
     }
 
-    onClicked = () => {
-        this._openModal();
+    imageProps = {
+        imageFit: ImageFit.contain,
+        height: 300,
+        width: 400,
+        className: "ModalImage"
+    }
+
+    footerCardSectionStyles = {
+        root: {
+          borderTop: '1px solid #F3F2F1'
+        }
     };
+
+    iconButtonStyles = mergeStyleSets({
+        root: {
+          color: theme.palette.neutralPrimary,
+          marginLeft: 'auto',
+          marginTop: '4px',
+          marginRight: '2px'
+        },
+        rootHovered: {
+          color: theme.palette.neutralDark
+        }
+      });
+      
+    headerStyles = {
+        root: {
+            fontSize: FontSizes.xxLargePlus,
+            fontWeight: FontWeights.bold,
+            padding: '12px 12px 14px 24px'
+        }
+    }
 
     render() {
         return <>
-        <Card onClick={this.onClicked} styles={this.galleryStyles}>
+        <Card onClick={this._openModal} styles={this.galleryStyles}>
             <Card.Item>
                 <Text variant="large" >
                     {this.props.data.title}
@@ -55,56 +78,49 @@ class GalleryCard extends React.Component {
                 <span/>
             </Card.Item>
 
-            <Card.Section styles={footerCardSectionStyles}>
-                <OverflowSet items={this.props.data.keys} onRenderItem={this._onRenderItem}/>
+            <Card.Section styles={this.footerCardSectionStyles}>
+                <OverflowSet items={this.props.data.keys} onRenderItem={this._onRenderKey}/>
             </Card.Section>
         </Card>
 
         <Modal isOpen={this.state.showModal} onDismiss={this._closeModal} className="GalleryModal">
-            <div>
-                <Text variant={"xxLargePlus"}>{this.props.data.title}</Text>
-                <OverflowSet items={this.props.data.imagepaths} onRenderItem={this._onRenderImage}/>
+            <div margin={200}>
+                <div>
+                    <Text styles={this.headerStyles}>{this.props.data.title}</Text>
+                    <IconButton
+                    styles={this.iconButtonStyles}
+                    iconProps={{ iconName: 'Cancel' }}
+                    ariaLabel="Close modal"
+                    onClick={this._closeModal}
+                    />
+                </div>
+                <DragScrollProvider>
+                    {({onMouseDown, ref}) => (
+                        <div className="ModalImages" ref={ref} onMouseDown={onMouseDown}>
+                            {this.props.data.imagepaths.map(src => (
+                                <Image {...this.imageProps} src={images(src)}/>
+                            ))}
+                        </div>
+                    )}  
+                </DragScrollProvider>
                 <OverflowSet vertical items={[this.props.data.paras[0]]} onRenderItem={this._onRenderModalPara}/>
-                <Text block variant={"xLarge"}>Relevant Skills</Text>
+                <Text block variant={"xLarge"} className="ModalPara">Relevant Skills</Text>
                 <OverflowSet vertical items= {this.props.data.keys} onRenderItem={this._onRenderModalKey}/>
-                <Link src={this.props.data.links[0]}>{this.props.data.links[0]}</Link>
+                <OverflowSet styles={this.footerCardSectionStyles} items={this.props.data.links} onRenderItem={this._onRenderLink} className="ModalLinks"/>
             </div>
         </Modal>
         </>
     }
 
-    _closeModal = () => {
-        this.setState({showModal: false});
-    }
+    /*
+        Card Functions
+    */
 
     _openModal = () => {
         this.setState({showModal: true});
     }
 
-    _onRenderLink = (link) => {
-        return(
-            <Link src={link}>{link}</Link>
-        )
-    }
-
-    _onRenderImage = (image) => {
-        var imageRef = images(image)
-        return (
-            <Image border={50} src={imageRef} height={450}/>
-        )
-    }
-
-    _onRenderModalKey = (key) => {
-        return (
-            <Text variant={'large'}>{"- " + key}</Text>
-        )
-    }
-
-    _onRenderModalPara = (para) => {
-        return <Text variant={'large'}>{para}</Text>
-    }
-
-    _onRenderItem = (item) => {
+    _onRenderKey = (item) => {
         return (
         <div className="GalleryKey">
           <Text variant={"xSmall"}>
@@ -113,8 +129,37 @@ class GalleryCard extends React.Component {
         </div>
         );
     };
+
+    /* 
+        Modal Functions
+    */
+
+    _closeModal = () => {
+        this.setState({showModal: false});
+    }
+
+    _onRenderLink = (link) => {
+        return(
+            <a href={link} className="ModalLink">{link}</a>
+        )
+    }
+
+    _onRenderImage = (image) => {
+        var imageRef = images(image)
+        return (
+            <Image {...this.imageProps} src={imageRef}/>
+        )
+    }
+
+    _onRenderModalKey = (key) => {
+        return (
+            <Text variant={'large'} className="ModalKey">{"- " + key}</Text>
+        )
+    }
+
+    _onRenderModalPara = (para) => {
+        return <Text variant={'large'} className="ModalPara">{para}</Text>
+    }
 }
-
-
 
 export default GalleryCard;
