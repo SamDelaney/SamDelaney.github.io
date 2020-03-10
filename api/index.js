474f366b141d61a016296a5b8bc5fd421ab16e19
Sam Delaney <sam@thedelaneys.net>
2020-03-09 19:13:13 -0700

    prepare for zeit setup

---
+++ api/index.js
@@ -0,0 +1,55 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import nodemailer from 'nodemailer';
+import cors from 'cors';
+
+const app = express();
+
+const port = 4444;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors());
+
+app.listen(port, () => {
+  console.log('We are live on port 4444');
+});
+
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my api');
+})
+
+app.post('/api/v1', (req,res) => {
+  var data = req.body;
+
+var smtpTransport = nodemailer.createTransport({
+  service: 'Gmail',
+  port: 465,
+  auth: {
+    user: 'samdelaney.tech',
+    pass: '1208-4BCE'
+  }
+});
+
+var mailOptions = {
+  from: data.email,
+  to: 'sam@thedelaneys.net',
+  subject: data.subject,
+  html: `<p>Email: ${data.email}</p>
+          <p>Phone: ${data.phone}</p>
+          <p>${data.message}</p>`
+};
+
+smtpTransport.sendMail(mailOptions,
+(error, response) => {
+  if(error) {
+    res.send(error)
+  }else {
+    res.send('Success')
+  } 
+  smtpTransport.close();
+});
+
+})
