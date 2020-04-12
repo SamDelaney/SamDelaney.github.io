81fa4263673b872d1b68058a3e92658290333b4c
Sam Delaney <sam@thedelaneys.net>
2020-04-11 22:12:33 -0700

    restructure for Github hosting

---
+++ src/components/about.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import {Text, Image} from 'office-ui-fabric-react'
+import Portrait from '../images/SamPortrait.jpg'
+import '../App.css'
+
+class About extends React.Component {
+    render() {
+        return <div className="About">
+            <Image src={Portrait} width={350}/>
+            <AboutText />
+        </div>
+    }
+}
+
+const _paraStyles = {
+    root: {
+        marginTop: 15
+    }
+}
+
+const _aboutParas = ["I am a 22-year-old linguistically educated software developer from Seattle Washington with a B.S. in Computer Science and a B.A. in Applied Linguistics from Trinity Western University in Langley, British Columbia.",
+ "Through my internships and the competitions I have attended, I have had the opportunity to reach fluency in a wide range of software development skills related to agile development and project management for a variety of platforms.",
+ "In addition to my technical abilities, my experience as a leader has enabled me to develop humility, selflessness, communicative skills and decisive autonomy, qualities which I believe are valuable to any team, in any role.",
+ "Please let me know if you're interested in working with me, I'd love to hear from you."
+];
+
+const AboutText = () => (
+    <div>
+        <Text block variant={"xLargePlus"}>About Me</Text>
+        {_aboutParas.map(function(text) {
+            return <Text block variant={"large"} styles={_paraStyles}>{text}</Text>
+        }
+        )}
+    </div>
+);
+
+export default About;
