602571df43e56dc64130d1f033baee82c659069e
SamDelaney <sam@thedelaneys.net>
2023-10-18 14:41:47 -0700

    rolling logo divider

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 //ui libraries
-import {Text} from 'office-ui-fabric-react';
+import {Text, Image} from 'office-ui-fabric-react';
 
 //custom imports
 import GalleryCard from './gallerycard';
 
 //additional files
+import Logo from '../images/Logo3.svg';
 import Data from './portfoliodata.json'
 import './stylesheets/portfolio.css'
 
@@ -19,24 +20,32 @@ class Portfolio extends React.Component {
         }
     }
 
-    gallery = () => (
+    work = () => (
         <div className="Gallery">
             <GalleryCard data={Data.grabbit} theme={this.props.theme}/>
-            <GalleryCard data={Data.pwmb} theme={this.props.theme}/>
             <GalleryCard data={Data.paratext} theme={this.props.theme}/>
+            <GalleryCard data={Data.thecombine} theme={this.props.theme}/>
+            <GalleryCard data={Data.fieldworks} theme={this.props.theme}/>
+        </div>  
+      ); 
+      projects = () => (
+        <div className="Gallery">
+            <GalleryCard data={Data.pwmb} theme={this.props.theme}/>
+            <GalleryCard data={Data.reflex} theme={this.props.theme}/>
             <GalleryCard data={Data.samdelaney_tech} theme={this.props.theme}/>
             <GalleryCard data={Data.wolfgang} theme={this.props.theme}/>
-            <GalleryCard data={Data.clarifeye} theme={this.props.theme}/>
-            <GalleryCard data={Data.thecombine} theme={this.props.theme}/>
-            <GalleryCard data={Data.lite} theme={this.props.theme}/>
-            <GalleryCard data={Data.fieldworks} theme={this.props.theme}/>
+            {/* <GalleryCard data={Data.clarifeye} theme={this.props.theme}/>
+            <GalleryCard data={Data.lite} theme={this.props.theme}/> */}
         </div>  
       ); 
 
     render() {
         return <div className="DefaultComponentWrapper"> 
-            <Text block variant={"xLargePlus"} styles={this.portfolioHeaderStyles}>My Work</Text>
-            <this.gallery />
+            <Text block variant={"xLargePlus"} styles={this.portfolioHeaderStyles}>Professional Work</Text>
+            <this.work />
+            <Image src={Logo} width={75} className="Sliding-Logo"/>
+            <Text block variant={"xLargePlus"} styles={this.portfolioHeaderStyles}>My Projects</Text>
+            <this.projects />
         </div>
     }
 }
